f1cd2ec0f5b28e3885945ff1fc258a5380241f1b
nope <xarament@gmail.com>
2023-06-26 23:02:58 +0400

    add slugs url - year/month/day/slug

--- onemore_proj/blog/models.py
+++ onemore_proj/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 class PublishedManager(models.Manager):
@@ -13,7 +14,7 @@ class Post(models.Model):
         Published = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
@@ -34,3 +35,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
